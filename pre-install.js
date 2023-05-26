18382135fb47547ce07fbbd21216b9f32e2a72fd
friselis <friselis@git.com>
2023-05-26 14:27:21 +0200

    release: build v0.1.8

--- pre-install.js
+++ pre-install.js
@@ -29,8 +29,8 @@ if (fs.existsSync(cargoDir)) {
     
 const features = process.env.npm_config_features ? `--features ${process.env.npm_config_features.replace(",", " ")}` : ""; 
 
-console.log(`Installing and compiling pg-migrust 0.1.7 ${features} ...`);
-exec(`cargo install pg-migrust --vers 0.1.7 ${features}`, (error, stdout, stderr) => {
+console.log(`Installing and compiling pg-migrust 0.1.8 ${features} ...`);
+exec(`cargo install pg-migrust --vers 0.1.8 ${features}`, (error, stdout, stderr) => {
   console.log(stdout);
   if (error || stderr) {
     console.log(error || stderr);
